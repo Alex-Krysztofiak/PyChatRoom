f6f8e1d3ade3b1da9d60a45ee2f9f9cdd77e8760
Alex Krysztofiak <128325501+Alex-Krysztofiak@users.noreply.github.com>
2023-09-02 10:53:41 -0400

    Add files via upload

---
+++ PySockets/functions.py
@@ -0,0 +1,13 @@
+import random
+from string import ascii_uppercase
+
+rooms = {}
+def generate_unique_code(length):
+    while True:
+        code = ""
+        for _ in range(length):
+            code += random.choice(ascii_uppercase)
+        if code not in rooms:
+            break
+
+    return code
